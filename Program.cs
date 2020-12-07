985c75f1b7f5067a3e0dc511fe98c232171c5a81
Luélly Cabral <luellycabral21@gmail.com>
2020-12-07 20:52:15 -0300

    adiciona a resposta do exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hora_Sistema_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime HoraAtual; 
+
+            HoraAtual = DateTime.Now;
+            Console.Write("Data e Hora atual: ");
+            Console.WriteLine($"{HoraAtual}");
+            Console.WriteLine();
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+
+        }
+    }
+}
